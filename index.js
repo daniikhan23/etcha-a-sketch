a4bac515456e34811ee50a11c60bb4913a12a3eb
Daniyal Ashraf Khan <dani.ashraf@outlook.com>
2023-08-21 15:23:14 +0500

    Added a title to the page as well as background color

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+function createGrid(num) {
+
+  const container = document.createElement('div');
+  container.className = 'container';
+  document.body.appendChild(container);
+
+  // create rows
+  for (let i = 0; i < num; i++) {
+    const row = document.createElement('div');
+    row.className = 'row';
+    container.appendChild(row);
+
+    //create columns and append them to the row
+    for (let j = 0; j < num; j++) {
+      const col = document.createElement('div');
+      col.className = 'col';
+      row.appendChild(col);
+
+      // using IIFE to add hover effect to each column immediately
+      (function (column) {
+        col.addEventListener('mouseover', () => {
+          const color = randomColor();
+          column.style.backgroundColor = color;
+        });
+      })
+      (col);
+    }
+  }
+}
+
+createGrid(8);
+
 function randomColor() {
   const letters = "0123456789ABCDEF";
   let color = "#";
@@ -7,33 +39,10 @@ function randomColor() {
   return color;
 }
 
-const container = document.createElement('div');
-container.className = 'container';
-document.body.appendChild(container);
+const btnGrid = document.querySelector('.btn-grid');
 
-// create rows
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement('div');
-  row.className = 'row';
-  container.appendChild(row);
+btnGrid.addEventListener('click', () => {
+  let num = parseInt(prompt("What size grid would you like?"));
+  createGrid(num);
+});
 
-  //create columns and append them to the row
-  for (let j = 0; j < 16; j++) {
-    const col = document.createElement('div');
-    col.className = 'col';
-    row.appendChild(col);
-
-    // using IIFE to add hover effect to each column immediately
-    (function (column) {
-      col.addEventListener('mouseover', () => {
-        const color = randomColor();
-        column.style.backgroundColor = color;
-      });
-
-      col.addEventListener('mouseout', () => {
-        column.style.backgroundColor = '';
-      });
-    })
-    (col);
-  }
-}
