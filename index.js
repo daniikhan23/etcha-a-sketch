4697085ace9a5c13bbf9e2b77fe64a4327db6c8e
Daniyal Ashraf Khan <dani.ashraf@outlook.com>
2023-08-21 14:43:48 +0500

    Added a hover effect to each column

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const container = document.createElement('div');
-container.id = 'container';
+container.className = 'container';
 document.body.appendChild(container);
-
-
 
 // create rows
 for (let i = 0; i < 16; i++) {
   const row = document.createElement('div');
-  row.id = 'row';
+  row.className = 'row';
   container.appendChild(row);
 
   //create columns and append them to the row
-  for (let i = 0; i < 16; i++) {
-    const col = document.createElement('div');
-    col.id = 'col';
+  for (let j = 0; j < 16; j++) {
+    var col = document.createElement('div');
+    col.className = 'col';
     row.appendChild(col);
+
+    // using IIFE to add hover effect to each column immediately
+    (function (column) {
+      col.addEventListener('mouseover', () => {
+        column.style.backgroundColor = 'red';
+      });
+
+      col.addEventListener('mouseout', () => {
+        column.style.backgroundColor = '';
+      });
+    })
+    (col);
   }
 }
